fb7dce6427b125ccd63170fdaf324c6f8ddd84af
leo1piece <fisherinaus@gmail.com>
2023-09-11 16:31:09 +1000

    contact me done

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -288,28 +288,28 @@ export const testimonial: TestimonialSection = {
  */
 
 export const contact: ContactSection = {
-  headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  headerText: 'Contact Me By Linkedin.',
+  description: '',
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'fisherinaus@gmail.com',
+      href: 'mailto:fisherinaus@gmail.com',
     },
+    // {
+    //   type: ContactType.Location,
+    //   text: 'Victoria BC, Canada',
+    //   href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+    // },
+    // {
+    //   type: ContactType.Instagram,
+    //   text: '@tbakerx',
+    //   href: 'https://www.instagram.com/tbakerx/',
+    // },
     {
-      type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
-    },
-    {
-      type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
-    },
-    {
-      type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      type: ContactType.LinkedIn,
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/leo-liuy',
     },
   ],
 };
