b79e8567341eba11ceffa25865fcf50052d683ed
leo1piece <fisherinaus@gmail.com>
2023-09-11 16:14:11 +1000

    noused value reduce

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -63,7 +63,7 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `Marvel Leo`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
